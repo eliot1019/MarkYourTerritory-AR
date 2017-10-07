109899a0ced89a00168626daeac84bb469581123
Eliot Han <eliothan@berkeley.edu>
2017-10-07 13:26:14 -0700

    Installed GeoFire. Upgrade Firebase pod to latest

--- MarkYourTerritory-AR/Utilities.swift
+++ MarkYourTerritory-AR/Utilities.swift
@@ -13,7 +13,7 @@ struct Utilities {
     
     /// Returns the height needed to display x text for a width and a font
     static func getHeight(toDisplay text: String, width: CGFloat, font: UIFont) -> CGFloat{
-        let attributes = [NSFontAttributeName: font]
+        let attributes = [NSAttributedStringKey.font: font]
         let rect = NSString(string: text).boundingRect(
             with: CGSize(width: width, height: CGFloat.greatestFiniteMagnitude),
             options: NSStringDrawingOptions.usesLineFragmentOrigin,
